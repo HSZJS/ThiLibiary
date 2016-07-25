f42acbe8a131d84cc552076353ebcb391ef89786
HSZJS <heshuzhan@heshuzhandeMacBook-Pro.local>
2016-07-25 16:02:30 +0800

    ThreeTest
    cocoa pods for github test

---
+++ ThiLibiaryTests/ThiLibiaryTests.m
@@ -0,0 +1,39 @@
+//
+//  ThiLibiaryTests.m
+//  ThiLibiaryTests
+//
+//  Created by 何舒展 on 16/7/25.
+//  Copyright © 2016年 何舒展. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ThiLibiaryTests : XCTestCase
+
+@end
+
+@implementation ThiLibiaryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
